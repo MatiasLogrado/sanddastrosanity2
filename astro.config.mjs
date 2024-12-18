c3faf491bef31e5a121122e16f1ddf8ad734453a
Loma <matiaslogrado@gmail.com>
2024-12-18 22:49:05 +0100

    add sanity ( with bug )

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    sanity({
+    projectId: "3etbwnl5",
+    dataset: "production",
+    useCdn: false, // for static builds
+    })
+]
 });
